65bc2819178a105735ef62b067cdcdef45d8900f
udit jain <uditj66@gmail.com>
2025-10-02 02:32:41 +0530

    middleware size reduces

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./lib/auth";
-import aj from "./lib/arcjet";
-import { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import aj from "./lib/arcjet";
+// import { createMiddleware, detectBot, shield } from "@arcjet/next";
 
 export async function middleware(req: NextRequest, res: NextResponse) {
   const session = await auth.api.getSession({
@@ -14,13 +14,13 @@ export async function middleware(req: NextRequest, res: NextResponse) {
   return NextResponse.next();
 }
 // Bot protection
-const validate = aj.withRule(shield({ mode: "LIVE" })).withRule(
-  detectBot({
-    mode: "LIVE",
-    allow: ["CATEGORY:SEARCH_ENGINE", "GOOGLE_CRAWLER"],
-  })
-);
-export default createMiddleware(validate);
+// const validate = aj.withRule(shield({ mode: "LIVE" })).withRule(
+//   detectBot({
+//     mode: "LIVE",
+//     allow: ["CATEGORY:SEARCH_ENGINE", "GOOGLE_CRAWLER"],
+//   })
+// );
+// export default createMiddleware(validate);
 
 // Generally we define routes in matcher array where we want to run middleware ,but there is new pattern coming around ,where we define the routes in matcher array where we don't want middleware to run.Below is the same pattern
 export const config = {
